154544197658a20e3823d996e73d8a49028076a8
JG Carvalho <jg@jgcarv.com>
2020-09-24 16:27:45 -0300

    updatet usingTellor

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 const mnemonic = process.env.ETH_MNEMONIC;
@@ -6,28 +6,28 @@ const mnemonic = process.env.ETH_MNEMONIC;
 const accessToken = process.env.WEB3_INFURA_PROJECT_ID;
 
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*",
-      gas: 6721975, // default ganache-cli value
-    },
-    rinkeby: {
-      provider: () =>
-        new HDWalletProvider(process.env.PRIVATE_KEY, `https://rinkeby.infura.io/v3/${accessToken}`),
-      network_id: 4,
-    },
-    mainnet: {
-      provider: () =>
-        new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${accessToken}`),
-      network_id: 1,
-      gas: 4700000,
-      gasPrice: 4000000000,
-    },
-    mocha: {
-      enableTimeouts: false,
-      before_timeout: 210000, // Here is 2min but can be whatever timeout is suitable for you.
-    },
-  }
+  // networks: {
+  //   development: {
+  //     host: "localhost",
+  //     port: 8545,
+  //     network_id: "*",
+  //     gas: 6721975, // default ganache-cli value
+  //   },
+  //   rinkeby: {
+  //     provider: () =>
+  //       new HDWalletProvider(process.env.PRIVATE_KEY, `https://rinkeby.infura.io/v3/${accessToken}`),
+  //     network_id: 4,
+  //   },
+  //   mainnet: {
+  //     provider: () =>
+  //       new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${accessToken}`),
+  //     network_id: 1,
+  //     gas: 4700000,
+  //     gasPrice: 4000000000,
+  //   },
+  //}
+  mocha: {
+    enableTimeouts: false,
+    before_timeout: 210000, // Here is 2min but can be whatever timeout is suitable for you.
+  },
 };
