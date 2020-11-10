556381ad89833b6c66d8c047776af0340ed5fce1
JG Carvalho <jg@jgcarv.com>
2020-11-10 19:33:03 -0300

    mock -> tellorPlayground

--- migrations/2_sampleUsingTellor.js
+++ migrations/2_sampleUsingTellor.js
@@ -1,18 +1,18 @@
 const Migrations = artifacts.require("Migrations");
 const SampleUsingTellor = artifacts.require("SampleUsingTellor");
-const MockTellor = artifacts.require("MockTellor");
+const TellorPlayground = artifacts.require("TellorPlayground");
 
 const mainnetAddress = "0x0Ba45A8b5d5575935B8158a88C631E9F9C95a2e5";
-const rinkebyAddress = "0xFe41Cb708CD98C5B20423433309E55b53F79134a";
+const testAddress = "0x20374E579832859f180536A69093A126Db1c8aE9"
 
 module.exports = function (deployer, network) {
   if (network == "mainnet") {
     deployer.deploy(SampleUsingTellor, mainnetAddress);
-  } else if (network == "rinkeby") {
-    deployer.deploy(SampleUsingTellor, rinkebyAddress);
+  } else if (network == "rinkeby" || network == "goerli" || network == "kovan" || network == "ropsten") {
+    deployer.deploy(SampleUsingTellor, testAddress);
   } else {
-    //deploy MockTellor
-    deployer.deploy(MockTellor, [], []).then((instance) => {
+    //deploy TellorPlayground
+    deployer.deploy(TellorPlayground, [], []).then((instance) => {
       deployer.deploy(SampleUsingTellor, instance.address);
     });
   }
