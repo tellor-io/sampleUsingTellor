0106850c95da5c597bcbf1de7301f9704605d0ca
lauren gregory <lmg2@illinois.edu>
2021-12-07 15:51:09 -0600

    upgraded syntax to tellorX
    changed requests to queries and edited types, passed test

--- contracts/SampleUsingTellor.sol
+++ contracts/SampleUsingTellor.sol
@@ -1,31 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0;
+pragma solidity >=0.8.0;
 
 import "usingtellor/contracts/UsingTellor.sol";
 
 contract SampleUsingTellor is UsingTellor {
     constructor(address payable _tellorAddress) UsingTellor(_tellorAddress) {}
 
-    function readTellorValue(uint256 _tellorID)
+    function readTellorValue(bytes32 _queryId)
         external
         view
-        returns (uint256)
+        returns (bytes memory)
     {
         //Helper function to get latest available value for that Id
-        (bool ifRetrieve, uint256 value, ) =
-            getCurrentValue(_tellorID);
-        if (!ifRetrieve) return 0;
-        return value;
+        (bool ifRetrieve, bytes memory _value, ) =
+            getCurrentValue(_queryId);
+        if (!ifRetrieve) return "0x";
+        return _value;
     }
 
-    function readTellorValueBefore(uint256 _tellorId, uint256 _timestamp)
+    function readTellorValueBefore(bytes32 _queryId, uint256 _timestamp)
         external view
-        returns (uint256, uint256)
+        returns (bytes memory, uint256)
     {
         //Helper Function to get a value before the given timestamp
-        (bool _ifRetrieve, uint256 _value, uint256 _timestampRetrieved) =
-            getDataBefore(_tellorId, _timestamp);
-        if (!_ifRetrieve) return (0, 0);
+        (bool _ifRetrieve, bytes memory _value, uint256 _timestampRetrieved) =
+            getDataBefore(_queryId, _timestamp);
+        if (!_ifRetrieve) return ("0x", 0);
         return (_value, _timestampRetrieved);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,6 +17,6 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.3",
 };
 
