5590e7195a3672a25582a752d34cc42d30c4e31f
tkernell <tpkernell@gmail.com>
2022-12-30 10:28:07 -0600

    check that data is not too old

--- contracts/SampleUsingTellor.sol
+++ contracts/SampleUsingTellor.sol
@@ -17,10 +17,13 @@ contract SampleUsingTellor is UsingTellor {
         // Retrieve data at least 15 minutes old to allow time for disputes
         (bytes memory _value, uint256 _timestampRetrieved) =
             getDataBefore(queryId, block.timestamp - 15 minutes);
-        // If data is available, parse it
+        // If timestampRetrieved is 0, no data was found
         if(_timestampRetrieved > 0) {
-            // Use the helper function _sliceUint to parse the bytes to uint256
-            ethPrice = _sliceUint(_value);
+            // Check that the data is not too old
+            if(block.timestamp - _timestampRetrieved < 24 hours) {
+                // Use the helper function _sliceUint to parse the bytes to uint256
+                ethPrice = _sliceUint(_value);
+            }
         }
     }
 }
